04ca8c77813a0b5bad3b716dc5ffafb2bd7d7f66
leaahl <leaahl@gmail.com>
2018-02-20 15:34:45 +0800

    added sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to your new account!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  resources :users
   resources :restaurants
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
