bcfe09502a7a2fa0afc07214e68b251dea68b389
leaahl <36429638+leaahl@users.noreply.github.com>
2018-02-20 10:40:43 +0800

    code review 1 (#2)
    * code review 1
    
    * added ratings and shown on page

--- app/controllers/restaurant_controller.rb
+++
@@ -1,4 +0,0 @@
-class RestaurantController < ApplicationController
-  def Home
-  end
-end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,3 @@
 class Restaurant < ApplicationRecord
+  validates :rating, numericality: { less_than_or_equal_to: 5,  only_integer: true }
 end
