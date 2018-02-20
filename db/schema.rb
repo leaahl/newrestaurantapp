214ce526058f62d8b0744f32ed504ac812f0310b
leaahl <leaahl@gmail.com>
2018-02-20 12:10:27 +0800

    added modelling user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220021718) do
+ActiveRecord::Schema.define(version: 20180220040701) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20180220021718) do
     t.datetime "updated_at", null: false
     t.integer "Ratings"
     t.integer "rating"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
